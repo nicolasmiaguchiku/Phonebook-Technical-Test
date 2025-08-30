65605cec2b5874c1d215971768114f909ba13914
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2025-08-30 03:54:05 -0300

    Contact post and fluentValidation working

--- Source/Phonebook.WebApi/Program.cs
+++ Source/Phonebook.WebApi/Program.cs
@@ -1,6 +1,7 @@
-using Phonebook.Application.Handlers;
+using Phonebook.Application.Extensions;
 using Phonebook.Domain.Interfaces;
 using Phonebook.Infrastructure.Extensions;
+
 using Phonebook.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,10 +15,10 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDataMongo(builder.Configuration);
 
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssembly(typeof(CreateContactHandler).Assembly);
-});
+builder.Services.AddApplicationServices();
+
+builder.Services.AddValidator();
+
 
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
