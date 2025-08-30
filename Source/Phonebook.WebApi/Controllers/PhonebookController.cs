da0d008decd47341545101d6d46da306fc42eefa
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2025-08-30 04:36:58 -0300

    GetAllContacts with handler working

--- Source/Phonebook.WebApi/Controllers/PhonebookController.cs
+++ Source/Phonebook.WebApi/Controllers/PhonebookController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MediatR;
-using Phonebook.Shared.Results;
-using Phonebook.Domain.Entities;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Phonebook.Application.Commands;
+using Phonebook.Application.Queries.GetAllContacts;
+using Phonebook.Domain.Entities;
+using Phonebook.Shared.Results;
 
 
 namespace Phonebook.WebApi.Controllers
@@ -24,11 +25,19 @@ namespace Phonebook.WebApi.Controllers
             return Ok("Deu certo");
         }
 
-        [HttpPost]
+        [HttpPost("AddContact-Phonebook")]
         public async Task<IActionResult> Create([FromBody] CreateContactCommand command)
         {
             ResultData<Contact> result = await _mediator.Send(command);
 
+            return Ok(result);
+        }
+
+
+        [HttpGet("GetAllContact-Phonebook")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllContactsQuery());
             return Ok(result);
         }
 
